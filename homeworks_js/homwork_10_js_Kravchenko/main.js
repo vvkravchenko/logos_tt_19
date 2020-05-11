20429471980e331adb2c8cffb4973fa51949bca7
vvkravchenko <63806106+vvkravchenko@users.noreply.github.com>
2020-05-11 16:07:27 +0300

    add: homwork_10_js_Kravchenko

---
+++ homeworks_js/homwork_10_js_Kravchenko/main.js
@@ -0,0 +1,48 @@
+// 1
+// const arr1 = new Array (1, 2, 3);
+// const arr2 = new Array (1, 2, 3, [10, 20]);
+
+// function arrCopy(arr){
+// return arr.slice();
+// }
+
+// let arr1Copy = arrCopy(arr1);
+// let arr2Copy = arrCopy(arr2);
+
+// console.log(arr1Copy);
+// console.log(arr2Copy);
+
+// 2
+// function arrToString(arr){
+// return arr.join();
+// }
+// const arr1 = arrToString([1,2,3]);
+// const arr2 = arrToString([10,200,3333]);
+
+// console.log(arr1);
+// console.log(arr2);
+
+// 3
+// function getLength(arr){
+//     let clone = arr.slice();
+//     for (let key in clone){
+//         clone[key] = clone[key].length;
+//         // key = key.length; // чому не працює такий синтаксис у циклі for of???
+//     }
+//     return clone;
+// }
+// const arr1 = getLength(['Ivan','Pavlo','Ira']); 
+// const arr2 = getLength(['Oleksiy','Andriana']); 
+
+// console.log(arr1);
+// console.log(arr2);
+
+// 4
+// function removeDuplicates(arr){
+//     return [...new Set(arr)];
+// }
+// const arr1 = removeDuplicates(['html','css','html','js']); 
+// const arr2 = removeDuplicates(['html','css','js','html','js','python','js','scss']);
+//  console.log(arr1);
+//  console.log(arr2);
+
